44a88314f07ba9e17c9c0f271625c5ec0a8f92d2
JinRun Huang <104927317+willhuang97@users.noreply.github.com>
2023-10-24 23:18:50 -0400

    Add files via upload

---
+++ CoffeeBeanSales.sql
@@ -0,0 +1,241 @@
+
+
+-- Understanding the tables
+
+SELECT *
+FROM [Personal Projects].dbo.customers$
+
+SELECT *
+FROM [Personal Projects].dbo.orders$
+
+SELECT *
+FROM [Personal Projects].dbo.products$
+
+-- Reformatting 'Order Date' into a date formate
+
+SELECT [Order Date], CONVERT(DATE,[Order Date])
+FROM [Personal Projects].dbo.orders$
+
+ALTER TABLE [Personal Projects].dbo.orders$
+ALTER COLUMN [Order Date] DATE
+
+-- Joining the table together
+
+SELECT *
+FROM [Personal Projects].dbo.customers$ AS c
+LEFT JOIN [Personal Projects].dbo.orders$ AS o
+ON c.[Customer ID] = o.[Customer ID]
+LEFT JOIN [Personal Projects].dbo.products$ AS p
+ON o.[Product ID] = p.[Product ID]
+
+-- Creating TEMP Table with the result from the Join query
+
+DROP TABLE if exists #CoffeeSales
+CREATE TABLE #CoffeeSales
+(
+[Customer ID] nvarchar(255),
+[Customer Name] nvarchar(255),
+[City] nvarchar(255),
+[Country] nvarchar(255),
+[Loyalty Card] nvarchar(255),
+[Order ID] nvarchar(255),
+[Order Date] date,
+[Product ID] nvarchar(255),
+[Quantity] nvarchar(255),
+[Coffee Type] nvarchar(255),
+[Roast Type] nvarchar(255),
+[Size] nvarchar(255),
+[Unit Price] float,
+
+[Profit] float
+)
+
+INSERT INTO #CoffeeSales
+SELECT 
+c.[Customer ID],
+c.[Customer Name],
+c.[City],
+c.[Country],
+c.[Loyalty Card],
+o.[Order ID],
+o.[Order Date],
+o.[Product ID],
+o.[Quantity],
+p.[Coffee Type],
+p.[Roast Type],
+p.[Size],
+p.[Unit Price],
+
+p.[Profit]
+FROM [Personal Projects].dbo.customers$ AS c
+LEFT JOIN [Personal Projects].dbo.orders$ AS o
+ON c.[Customer ID] = o.[Customer ID]
+LEFT JOIN [Personal Projects].dbo.products$ AS p
+ON o.[Product ID] = p.[Product ID]
+WHERE [Order Date] IS NOT NULL
+
+-------------------------------------------------------
+
+-- A look at the data within temp table #CoffeeSales
+
+SELECT *
+FROM #CoffeeSales
+
+-------------------------------------------------------
+
+-- What is the total revenue and profit?
+
+SELECT ROUND(SUM([Quantity] * [Unit Price]),2) AS total_revenue, ROUND(SUM(Profit),2) AS Profit
+FROM #CoffeeSales
+
+-------------------------------------------------------
+
+-- What is the total revenue each year? 
+
+SELECT YEAR([Order Date]) AS Order_Year, ROUND(SUM([Quantity] * [Unit Price]),2) AS total_revenue
+FROM #CoffeeSales
+GROUP BY YEAR([Order Date])
+
+-------------------------------------------------------
+
+-- What is the average revenue for each product category (Product ID) and What product had the highest revenue?
+
+WITH ProductRev AS (
+  SELECT
+    [Product ID],
+    ROUND(SUM([Quantity] * [Unit Price]), 2) AS total_revenue
+  FROM #CoffeeSales
+  GROUP BY [Product ID] 
+)
+SELECT
+  [Product ID],
+  total_revenue,
+  AVG(total_revenue) OVER (PARTITION BY [Product ID]) AS Average_Revenue
+FROM ProductRev
+ORDER BY Average_Revenue DESC
+
+
+-------------------------------------------------------
+
+-- What is the average revenue for each product category (Product ID) in conjunction to its profit
+
+WITH ProductRev AS (
+  SELECT
+    [Product ID],
+    ROUND(SUM([Quantity] * [Unit Price]), 2) AS total_revenue
+  FROM #CoffeeSales
+  GROUP BY [Product ID]
+),
+ProductAvg AS (
+SELECT
+  [Product ID],
+  total_revenue,
+  AVG(total_revenue) OVER (PARTITION BY [Product ID]) AS Average_Revenue
+FROM ProductRev
+ 
+)
+SELECT
+  pa.[Product ID],
+  MAX(pa.Average_Revenue) AS Average_Revenue,
+  ROUND(SUM(cs.Profit),2) AS Total_Profit,
+  CASE
+	WHEN SUM(cs.Profit) >= 80 THEN 'High Profit'
+	WHEN SUM(cs.Profit) >= 50 THEN 'Moderate Profit'
+	ELSE 'Low Profit'
+	END AS Profit_Category
+FROM ProductAvg pa
+JOIN #CoffeeSales cs ON pa.[Product ID] = cs.[Product ID]
+GROUP BY pa.[Product ID]
+ORDER BY MAX(pa.Average_Revenue) DESC
+
+-------------------------------------------------------
+
+-- What is the most popular Roast Type? 
+
+SELECT COUNT(*), [Roast Type]
+FROM #CoffeeSales
+GROUP BY [Roast Type]
+
+-------------------------------------------------------
+
+-- What is the most popular Coffee Type? 
+
+SELECT COUNT(*), [Coffee Type]
+FROM #CoffeeSales
+GROUP BY [Coffee Type]
+
+-------------------------------------------------------
+
+-- What is the sales like between loyalty and non-loyalty 
+SELECT
+	[Loyalty Card],
+	ROUND(SUM([Quantity] * [Unit Price]),2) AS TotalSales,
+	ROUND(SUM(Profit),2) AS TotalProfit
+FROM #CoffeeSales
+GROUP BY [Loyalty Card];
+
+-------------------------------------------------------
+
+-- What is the sales and profit like countries?
+SELECT
+	[Country],
+	ROUND(SUM([Quantity] * [Unit Price]),2) AS TotalSales,
+	ROUND(SUM(Profit),2) AS TotalProfit
+FROM #CoffeeSales
+GROUP BY [Country];
+
+-------------------------------------------------------
+
+-- Creating a table for easier dashboard building via PowerBI 
+
+CREATE TABLE CoffeeSalesData
+(
+    [Customer ID] nvarchar(255),
+    [Customer Name] nvarchar(255),
+    [City] nvarchar(255),
+    [Country] nvarchar(255),
+    [Loyalty Card] nvarchar(255),
+    [Order ID] nvarchar(255),
+    [Order Date] date,
+    [Product ID] nvarchar(255),
+    [Quantity] int,
+    [Coffee Type] nvarchar(255),
+    [Roast Type] nvarchar(255),
+    [Size] nvarchar(255),
+    [Unit Price] float,
+    [Profit] float
+)
+
+INSERT INTO CoffeeSalesData
+
+-- Below to be use in PowerBI
+SELECT 
+c.[Customer ID],
+c.[Customer Name],
+c.[City],
+c.[Country],
+c.[Loyalty Card],
+o.[Order ID],
+o.[Order Date],
+o.[Product ID],
+o.[Quantity],
+p.[Coffee Type],
+p.[Roast Type],
+p.[Size],
+p.[Unit Price],
+
+p.[Profit]
+FROM [Personal Projects].dbo.customers$ AS c
+LEFT JOIN [Personal Projects].dbo.orders$ AS o
+ON c.[Customer ID] = o.[Customer ID]
+LEFT JOIN [Personal Projects].dbo.products$ AS p
+ON o.[Product ID] = p.[Product ID]
+WHERE [Order Date] IS NOT NULL
+
+
+-------------------------------------------------------
+
+-- Checking the table
+
+SELECT * 
+FROM CoffeeSalesData
--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# Coffee_Bean_Sales_Portfolio_Project
+
+
+![](images/mdTitle.png)
+
+
+
+### Project Description
+
+This project aims to examine coffee beans sales for a fictional coffee bean wholesaler. The sales data, provided by [Saad Haroon](https://www.kaggle.com/datasets/saadharoon27/coffee-bean-sales-raw-dataset) via Kaggle, covers the period from 2019 to 2022. The primary objective is to delve into this sales data, focusing on revenue, profit, and customer sales data. Through SQL and Power BI, we seek to uncover sales trends and offer valuable insights into the data."
+
+#### File Organization 
+
+*Notebook - SQL*
+- CoffeeBeanSales.sql
+
+*Dashboard - PowerBI*
+- CoffeeBeanSales.pbix
+
+*Data Files*
+- ~$Raw Data.xlsx
+- customer.xlsx
+- orders.xlsx
+- product.xlsx
+
+Data Source: https://www.kaggle.com/datasets/saadharoon27/coffee-bean-sales-raw-dataset
+
+![MicrosoftSQLServer](https://img.shields.io/badge/Microsoft%20SQL%20Server-CC2927?style=for-the-badge&logo=microsoft%20sql%20server&logoColor=white) ![Power Bi](https://img.shields.io/badge/power_bi-F2C811?style=for-the-badge&logo=powerbi&logoColor=black) ![Microsoft Excel](https://img.shields.io/badge/Microsoft_Excel-217346?style=for-the-badge&logo=microsoft-excel&logoColor=white)
+
+### Analysis
+
+#### Data Collection
+
+The Coffee Bean Sales raw dataset was provided by Kaggle user Saad Haroon. Initial **customer.xlsx**, **orders.xlsx**, and **product.xlsx** were all in one workbook call **~$Raw Data.xlsx**. To facilitate analysis in SQL Server, I used a Microsoft Excel VBA command to split these sheets into individual workbooks, which were then imported into SQL Server.
+
+#### SQL Analysis
+
+During the initial SQL analysis, the focus was on gaining an understanding of the data and performing some necessary reformatting. To enhance the readability of the analysis, the 'Order Date' column was reformatted from a DATETIME to a DATE data type.
+
+After formatting, all three tables joined together using a LEFT JOIN and resulting query was stored into a temporary table (**#CoffeeSales**). This table includes only the columns necessary for the analysis.
+
+From the **#CoffeeSales** table, I examined total revenue, profit as well as using PARTITION to identify products with the highest average revenue alongside their total revenue. 
+
+To better understand the profit margin withing each product, I created a CASE statement for a quick overview of products that were high profit, moderate profit and low profit. 
+
+Additionally, the analysis delved into understanding sales based on factors like loyalty card membership, countries, coffee types, and roast types. This provided valuable insights into the sources of sales and customer preferences.
+
+#### PowerBi Dashboard
+
+Below dashboard is created with the initial SQL join statement. 
+The dashboard includes a line graph comparing revenue and profit, a quick summary of revenue, profit and sales numbers, total revenue of each product and revenue breakdown for each category groups. 
+
+![](images/dashboard.png)
+
+
+
+Analyzing the dashboard, we observe that between 2019 and 2022, there were periods when profit was lower than revenue. This discrepancy may be attributed to higher operating costs. Sales remained consistent, with a peak occurring in late 2021 before a significant decline in sales around July 2022.
+
+Additionally, the majority of sales were from the United States and most coffee products generated revenue exceeding $4,000, with the exception of Robusta coffee beans, which fell below the average.
+
+The dashboard also highlights a nearly equal ratio between the percentage of loyalty cardholders and revenue earned from cardholders.
+
+From this dashboard, our fictional coffee bean wholesaler gains valuable insights into sales data. They can assess aspects such as the viability of maintaining a loyalty program, considering the operating costs involved. The funds saved from potential cost reductions might be strategically redirected toward marketing campaigns, especially in the U.S., which is the primary revenue source.
